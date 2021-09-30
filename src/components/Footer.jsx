4629aeab60fe9f38887449af6dfe4178956e194e
Alejandro Cabanzo <alejo.0704@hotmail.com>
2021-09-30 02:27:32 -0500

    Se realizan modificaciones en Navbar/Footer/stylesGeneral

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 
 const Footer = () => {
     return (
-        <footer>
-            Este es el footer de la pagina
+        <footer >
+            <div className='footer'>
+                <div className='contenedorDivsFooter'>
+                    <div>
+                        <span >Calle 16 # 43 - 76 Colombia / 3102224445 / mueblestic@hotmail.com</span>
+                    </div>
+                    <div >
+                        <span >Síguenos en Facebook</span>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
