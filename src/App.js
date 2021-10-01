0d714fcbd4f3b6b32b1a5f5d6fc6b2e19765cbfb
Angie M <lore.angie1312@gmail.com>
2021-09-30 21:38:54 -0500

    Se inició el registro

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InfoProductos from 'pages/InfoProductos';
+
+function App() {
+  return (
+    <div className="App">
+      <InfoProductos/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/RegistroProductos.jsx
@@ -0,0 +1,11 @@
+const RegistroProductos = () => {
+    return (
+        <div>
+            <h2>
+                Registro de productos
+            </h2>
+        </div>
+    );
+}
+
+export default RegistroProductos ;
